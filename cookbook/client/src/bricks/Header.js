7d318f0a51e5535dbb0a3f403d8364a2b522d757
StiboraTomas <stue08@gmail.com>
2023-10-25 23:16:02 +0200

    Implement fulltext search for both name and description

--- cookbook/client/src/bricks/Header.js
+++ cookbook/client/src/bricks/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../css/header.module.css";
+//Vypadá otřesně, hodil bych uplně pryč a nechal jen navbar
 
 function ShowHeader(props) {
 
--- cookbook/client/src/bricks/RecipeList.js
+++ cookbook/client/src/bricks/RecipeList.js
@@ -1,20 +1,60 @@
 import Navbar from "react-bootstrap/Navbar";
 import Button from "react-bootstrap/Button";
-import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import React, { useState, useMemo } from "react";
 import RecipeGridList from "./RecipeGridList";
 import RecipeTableList from "./RecipeTableList";
 import Icon from "@mdi/react";
-import { mdiTable, mdiViewGridOutline } from "@mdi/js";
+import { mdiTable, mdiViewGridOutline, mdiMagnify } from "@mdi/js";
+
 
 function VypisRecepty(props) {
   const [viewType, setViewType] = useState("grid");
   const isGrid = viewType === "grid";
+  const [searchBy, setSearchBy] = useState("");
+
+  const filteredRecipeList = useMemo(() => {
+    return props.recipeData.filter((item) => {
+      return (
+        item.name.toLocaleLowerCase().includes(searchBy.toLocaleLowerCase()) ||
+        item.description.toLocaleLowerCase().includes(searchBy.toLocaleLowerCase())
+      );
+    });
+  }, [searchBy]);
+
+  function handleSearch(event) { // the function that we will run when the form is "submitted"- i.e. when the search button is pressed
+    event.preventDefault();
+    setSearchBy(event.target["searchInput"].value);
+  }
+  
+  function handleSearchDelete(event) { // a function that will run when the lookup input value changes
+    if (!event.target.value) setSearchBy(""); // if there is no value in the input (user presses x), the search will be canceled
+  }
 
   return (
     <div>
       <Navbar>
         <div className="container-fluid">
           <Navbar.Brand>Seznam Receptů</Navbar.Brand>
+
+          <div>
+            <Form className="d-flex" onSubmit={handleSearch}>
+              <Form.Control
+                id={"searchInput"}
+                style={{ maxWidth: "150px" }}
+                type="search"
+                placeholder="Search"
+                aria-label="Search"
+                onChange={handleSearchDelete}
+              />
+              <Button
+                style={{ marginRight: "8px" }}
+                variant="outline-dark"
+                type="submit"
+              >
+                <Icon size={1} path={mdiMagnify} />
+              </Button>
+
           <Button
             variant="outline-dark"
             onClick={() =>
@@ -27,12 +67,14 @@ function VypisRecepty(props) {
             <Icon size={1} path={isGrid ? mdiTable : mdiViewGridOutline} />{" "}
             {isGrid ? "Změna zobrazení" : "Změna zobrazení"}
           </Button>
+          </Form>
+          </div>
         </div>
       </Navbar>
       {isGrid ? (
-        <RecipeGridList recipeData={props.recipeData} />
+        <RecipeGridList recipeData={filteredRecipeList} />
       ) : (
-        <RecipeTableList recipeData={props.recipeData} />
+        <RecipeTableList recipeData={filteredRecipeList} />
       )}
     </div>
   );
