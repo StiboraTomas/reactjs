7553af268346749906424964807a0e54d9bea02f
StiboraTomas <stue08@gmail.com>
2023-11-06 22:59:25 +0100

    5.1 - Make GridList cards responsive

--- cookbook/client/src/bricks/RecipeGridList.js
+++ cookbook/client/src/bricks/RecipeGridList.js
@@ -1,10 +1,24 @@
 import React from "react";
 import Recept from "./Recipe";
 
+
 function RecipeGridList(props) {
-  return props.recipeData.map((recept) => {
-    return <Recept key={recept.id} recept={recept} />;
-  });
+  return (
+  <div class="row">
+    {props.recipeData.map((recept) => {
+      return (
+        <div
+        class="col-12 col-sm-6 col-md-4 col-lg-4 col-xl-3"
+            style={{ paddingBottom: "16px" }}
+          >
+            <Recept key={recept.id} recept={recept} />
+          </div>
+
+      );
+})}
+  </div>
+  );
 }
+   
 
 export default RecipeGridList;
